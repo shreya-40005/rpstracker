0e743d92d65eea9a758784a5a0713dba189a448e
shreya-40005 <122539803+shreya-40005@users.noreply.github.com>
2023-01-31 10:17:33 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,61 @@
+function updateScoreboard () {
+    OLED.clear()
+    OLED.writeStringNewLine("Player 1 score: " + P1)
+    OLED.newLine()
+    OLED.writeStringNewLine("Player 2 score: " + P2)
+    OLED.newLine()
+    OLED.writeStringNewLine("Ties:" + Ties)
+    OLED.newLine()
+    OLED.writeStringNewLine("Rounds: " + Rounds)
+}
+input.onButtonPressed(Button.A, function () {
+    OLED.init(128, 64)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    basic.pause(2000)
+    basic.clearScreen()
+    basic.pause(1000)
+    P1 += 1
+    Rounds += 1
+    updateScoreboard()
+})
+input.onButtonPressed(Button.AB, function () {
+    OLED.init(128, 64)
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(2000)
+    basic.clearScreen()
+    basic.pause(1000)
+    Ties += 1
+    Rounds += 1
+    updateScoreboard()
+})
+input.onButtonPressed(Button.B, function () {
+    OLED.init(128, 64)
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # # . .
+        `)
+    basic.pause(2000)
+    basic.clearScreen()
+    basic.pause(1000)
+    P2 += 1
+    Rounds += 1
+    updateScoreboard()
+})
 input.onGesture(Gesture.Shake, function () {
     Reset()
 })
@@ -9,20 +67,10 @@ function Reset () {
     Rounds = 0
     OLED.writeStringNewLine("Shall we play a game?")
     basic.pause(2000)
-    OLED.clear()
-    OLED.writeStringNewLine("Player 1 score: " + P1)
-    OLED.newLine()
-    OLED.writeStringNewLine("Player 2 score: " + P2)
-    OLED.newLine()
-    OLED.writeStringNewLine("Ties:" + Ties)
-    OLED.newLine()
-    OLED.writeStringNewLine("Rounds: " + Rounds)
+    updateScoreboard()
 }
 let Rounds = 0
 let Ties = 0
 let P2 = 0
 let P1 = 0
 Reset()
-basic.forever(function () {
-	
-})
